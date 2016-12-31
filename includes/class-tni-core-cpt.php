31e4184b382d53d32688f1f8297ec37032ef4e9c
Pea Lutz <pea@glocal.coop>
2016-12-30 21:43:29 -0500

    Clean up comments.

--- includes/class-tni-core-cpt.php
+++ includes/class-tni-core-cpt.php
@@ -55,25 +55,6 @@ class TNI_Core_CPT {
             )
         );
 
-        // register_extended_post_type( 'av', array(
-        //     'taxonomies' => array( 'category' )
-        // ) );
-        //
-        // register_extended_post_type( 'features', array(
-        //     'taxonomies' => array( 'category' )
-        // ) );
-        //
-        // register_extended_post_type( 'essays', array(
-        //     'taxonomies' => array( 'category' )
-        // ) );
-        //
-        // register_extended_post_type( 'and-meanwhile', array(
-        //     'taxonomies' => array( 'category' )
-        // ) );
-        //
-        // register_extended_post_type( 'news', array(
-        //     'taxonomies' => array( 'category' )
-        // ) );
     }
 
     /**
