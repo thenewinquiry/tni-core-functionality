308cb77e4408482d964b6d233f98c390b3309e57
Pea <pea@misfist.com>
2017-04-11 16:39:17 -0400

    Updated template to use WP template tag

--- templates/related-posts.php
+++ templates/related-posts.php
@@ -12,19 +12,24 @@
 <div class="post-column clearfix">
   <article id="<?php get_the_ID(); ?>" class="related-post type-post">
     <figure class="post-thumbnail">
-      <a href="<?php echo esc_url( $permalink ); ?>" rel="bookmark">
-        <?php echo $thumb; ?>
+      <a href="<?php echo esc_url( the_permalink() ); ?>" rel="bookmark">
+        <?php if( has_post_thumbnail( $post ) ) : ?>
+          <?php the_post_thumbnail( 'thumbnail' ); ?>
+        <?php endif; ?>
       </a>
     </figure>
     <div class="entry-meta">
       <span class="meta-category"><a href="<?php echo esc_url( get_category_link( $category->term_id ) ); ?>" rel="category tag"><?php esc_html_e( $category->name ); ?> </a></span>
     </div>
     <h2 class="entry-title">
-      <a href="<?php echo esc_url( $permalink ); ?>" rel="bookmark">
-        <?php echo esc_attr( $related_post->post_title ); ?>
+      <a href="<?php echo esc_url( the_permalink() ); ?>" rel="bookmark">
+        <?php the_title(); ?>
       </a>
     </h2>
     <div class="entry-meta">
+    <?php if( function_exists( 'tni_core_authors' ) ) : ?>
+      <?php echo tni_core_authors(); ?>
+    <?php else : ?>
       <span class="meta-author">
         <span class="author vcard">
           <?php _e( 'By', 'tni-core' ); ?> <a href="<?php echo esc_url( $author_url ); ?>" class="url fn n">
@@ -32,6 +37,8 @@
           </a>
         </span>
       </span>
+    <?php endif; ?>
+
     </div>
     <div class="entry-content entry-excerpt clearfix">
       <?php echo esc_html_e( $subhead ); ?>
