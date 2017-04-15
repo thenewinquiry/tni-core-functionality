447ec04ef003ac2b1c3c7ae1a2a0d73b96826e42
Pea <pea@misfist.com>
2017-04-15 17:30:45 -0400

    Added authorization settings
    Added options page under Settings
    Added `authorization_base_url` option (access with `get_option( 'options_authorization_base_url' )`)
    Added method to set and get base_url value

--- includes/class-tni-core-authorization.php
+++ includes/class-tni-core-authorization.php
@@ -51,13 +51,20 @@ class TNI_Core_Authorization {
 	public $assets_url;
 
   /**
-   * Base URL
-   * Class constant TNI_Core_Authorization::BASEURL
+	 * The base authoriztion URL.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.9
+	 */
+  public $base_url;
+
+  /**
+   * The default authoriztion URL.
    * @var     string
    * @access  public
    * @since   1.0.9
    */
-  const BASEURL = 'https://members.thenewinquiry.com';
+  public $default_url;
 
   /**
    * Initialize all the things
@@ -75,6 +82,12 @@ class TNI_Core_Authorization {
     $this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+    $this->default_url = 'https://members.thenewinquiry.com';
+    $this->set_base_url();
+
+    $this->add_options_page();
+    $this->add_options_fields();
   }
 
   /**
@@ -87,10 +100,108 @@ class TNI_Core_Authorization {
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'tni-core-authentication', $this->assets_url . 'js/auth.js', array( 'jquery' ), $this->_version, true );
       $js_authorization = array(
-          'baseURL' => TNI_Core_Authorization::BASEURL,
+          'baseURL' => $this->base_url,
           'nonce'   => wp_create_nonce( 'tni_js_authorization' )
     );
     wp_localize_script( 'tni-core-authentication', 'jsAuthorization', $js_authorization );
 	}
+
+  /**
+   * Get Value of Class Variable
+   *
+   * @since 1.0.9
+   *
+   * @return string $this->base_url
+   */
+  public function get_base_url() {
+    return $this->base_url;
+  }
+
+  /**
+   * Set Value of Class Variable
+   *
+   * @since 1.0.9
+   *
+   * @return string $this->base_url
+   */
+  public function set_base_url() {
+    if( null == get_option( 'options_authorization_base_url' ) ) {
+      update_option( 'options_authorization_base_url', $this->default_url );
+    }
+    $this->base_url = get_option( 'options_authorization_base_url', $this->default_url );
+  }
+
+  /**
+   * Add Options Page
+   *
+   * @since 1.0.9
+   *
+   * @uses acf_add_options_page()
+   *
+   * @return void
+   */
+  public function add_options_page() {
+    if( function_exists( 'acf_add_options_page' ) ) {
+      acf_add_options_sub_page( array(
+    		'page_title' 	=> __( 'Authorization Settings', 'tni-core' ),
+    		'menu_title'	=> __( 'Authorization Settings', 'tni-core' ),
+    		'menu_slug' 	=> 'tni-authorization-settings',
+    		'parent_slug'	=> 'options-general.php'
+    	) );
+    }
+  }
+
+  /**
+   * Add Options Fields
+   *
+   * @since 1.0.9
+   *
+   * @uses acf_add_local_field_group()
+   *
+   * @return void
+   */
+  public function add_options_fields() {
+    if( function_exists( 'acf_add_local_field_group' ) ) {
+      acf_add_local_field_group( array (
+        'key' => 'group_authorization_settings',
+        'title' => __( 'Authorization Settings', 'tni-core' ),
+        'fields' => array (
+          array (
+            'key' => 'field_authorization_base_url',
+            'label' => __( 'Base URL', 'tni-core' ),
+            'name' => 'authorization_base_url',
+            'type' => 'url',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'default_value' => 'https://members.thenewinquiry.com',
+            'placeholder' => '',
+          ),
+        ),
+        'location' => array (
+          array (
+            array (
+              'param' => 'options_page',
+              'operator' => '==',
+              'value' => 'tni-authorization-settings',
+            ),
+          ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'seamless',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => 1,
+        'description' => '',
+      ));
+    }
+  }
 
 }
