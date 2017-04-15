9b17ab9ceadd5cf3b0fa921a499935b7922e9331
Pea <pea@misfist.com>
2017-04-15 17:40:10 -0400

    Added default url value to settings field

--- includes/class-tni-core-authorization.php
+++ includes/class-tni-core-authorization.php
@@ -106,5 +106,102 @@ class TNI_Core_Authorization {
     wp_localize_script( 'tni-core-authentication', 'jsAuthorization', $js_authorization );
 	}
 
+  /**
+   * Get Value of Class Variable
+   *
+   * @since 1.0.9
+   *
+   * @return string $this->base_url
+   */
+  public function get_base_url() {
+    return $this->base_url;
+  }
+
+  /**
+   * Set Value of Class Variable
+   *
+   * @since 1.0.9
+   *
+   * @return string $this->base_url
+   */
+  public function set_base_url() {
+    if( null == get_option( 'options_authorization_base_url' ) ) {
+      update_option( 'options_authorization_base_url', $this->default_url );
+    }
+    $this->base_url = get_option( 'options_authorization_base_url', $this->default_url );
+  }
+
+  /**
+   * Add Options Page
+   *
+   * @since 1.0.9
+   *
+   * @uses acf_add_options_page()
+   *
+   * @return void
+   */
+  public function add_options_page() {
+    if( function_exists( 'acf_add_options_page' ) ) {
+      acf_add_options_sub_page( array(
+    		'page_title' 	=> __( 'Authorization Settings', 'tni-core' ),
+    		'menu_title'	=> __( 'Authorization Settings', 'tni-core' ),
+    		'menu_slug' 	=> 'tni-authorization-settings',
+    		'parent_slug'	=> 'options-general.php'
+    	) );
+    }
+  }
+
+  /**
+   * Add Options Fields
+   *
+   * @since 1.0.9
+   *
+   * @uses acf_add_local_field_group()
+   *
+   * @return void
+   */
+  public function add_options_fields() {
+    if( function_exists( 'acf_add_local_field_group' ) ) {
+      acf_add_local_field_group( array (
+        'key' => 'group_authorization_settings',
+        'title' => __( 'Authorization Settings', 'tni-core' ),
+        'fields' => array (
+          array (
+            'key' => 'field_authorization_base_url',
+            'label' => __( 'Base URL', 'tni-core' ),
+            'name' => 'authorization_base_url',
+            'type' => 'url',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'default_value' => $this->default_url,
+            'placeholder' => '',
+          ),
         ),
+        'location' => array (
+          array (
+            array (
+              'param' => 'options_page',
+              'operator' => '==',
+              'value' => 'tni-authorization-settings',
+            ),
+          ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'seamless',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => 1,
+        'description' => '',
+      ));
+    }
+  }
+
 }
