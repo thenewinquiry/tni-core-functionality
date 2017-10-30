6572d2cad1cc5183858ac071cc8a5919ab0b5d02
Pea <pea@misfist.com>
2017-10-30 17:50:31 -0400

    #95 SEO customization for JetPack
    * Added filter to display custom meta if data provided 
    * Added seo image size

--- includes/jetpack-customization.php
+++ includes/jetpack-customization.php
@@ -15,6 +15,7 @@
   * If `seo_description` doesn't exist for a post/page, do not modify `og:description`.
   *
   * @since 1.1.1
+  * @since 1.3.1
   *
   * @uses jetpack_open_graph_tags filter
   * @uses get_post_meta
@@ -23,10 +24,28 @@
   * @return {array} $tags
   */
 function tni_core_og_description( $tags ) {
-  $seo_description = get_post_meta( get_the_id(), 'seo_description', true );
-  if( $seo_description ) {
-    $tags['og:description'] = esc_attr__( $seo_description, 'tni-core' );
+  if( $seo_description_facebook = get_post_meta( get_the_id(), 'seo_description', true ) ) {
+    $tags['og:description'] = esc_attr__( $seo_description_facebook, 'tni-core' );
+  }
+  if( $seo_description_twitter = get_post_meta( get_the_id(), 'seo_description_twitter', true ) ) {
+    $tags['twitter:description'] = esc_attr__( $seo_description_twitter, 'tni-core' );
+  }
+  if( $seo_image = get_post_meta( get_the_id(), 'seo_image', true ) ) {
+    unset( $tags['twitter:image'] );
+    unset( $tags['og:image'] );
+    $image_obj = wp_get_attachment_image_src( (int) $seo_image, 'seo' );
+    $tags['twitter:image'] = esc_url( $image_obj[0] );
+    $tags['og:image'] = esc_url( $image_obj[0] );
   }
   return $tags;
 }
-add_filter( 'jetpack_open_graph_tags', 'tni_core_og_description' );
+add_filter( 'jetpack_open_graph_tags', 'tni_core_og_description', 99 );
+
+/**
+ * Add SEO Image Size
+ *
+ * @since 1.3.1
+ *
+ * @link https://developers.facebook.com/docs/sharing/best-practices/#images
+ */
+add_image_size( 'seo', 1200, 1200, array( 'center', 'top' ) );
